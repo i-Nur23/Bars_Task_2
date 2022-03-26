35b1922198b50431897574a519a5ff5e6ed5bc66
Ainur Iskhakov <94246600+i-Nur23@users.noreply.github.com>
2022-03-27 00:55:40 +0300

    Добавьте файлы проекта.

---
+++ Task2_Collections/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+namespace Program;
+internal static class Program
+{
+    private static void Main()
+    {
+
+        Entity EntityRoot = new Entity(1, "Root entity", 0);
+        Entity Entity1 = new Entity(2, "Child of 1 entity", 1);
+        Entity Entity2 = new Entity(3, "Child of 1 entity", 1);
+        Entity Entity3 = new Entity(4, "Child of 2 entity", 2);
+        Entity Entity4 = new Entity(5, "Child of 4 entity", 4);
+
+        List<Entity> entitiesList = new List<Entity>() { EntityRoot, Entity1, Entity2, Entity3, Entity4 };
+
+        Dictionary<int, List<Entity>> entitiesDict = ConvertToDict(entitiesList);
+
+        foreach (var item in entitiesList)
+        {
+            Console.WriteLine($"Entity{{Id = {item.Id}, ParentId = {item.ParentId}, Name = {item.Name}}}");
+        }
+
+        Console.WriteLine("==============");
+
+        foreach (var keys in entitiesDict)
+        {
+            Console.Write($"Key : {keys.Key}, Value: List{{");
+            foreach (var entities in keys.Value)
+            {
+                Console.Write($"Entity{{Id = {entities.Id}}} ");
+            }
+            Console.WriteLine("}");
+        }
+    }
+
+    public static Dictionary<int, List<Entity>> ConvertToDict(List<Entity> list)
+    {
+        Dictionary<int, List<Entity>> newDict = new Dictionary<int, List<Entity>>();
+
+        List<Entity> entities = new List<Entity>();
+
+        foreach (Entity entity in list)
+        {
+            if (newDict.TryGetValue(entity.ParentId, out List<Entity> entitiesFromOneParents))
+            {
+                if (!entitiesFromOneParents.Contains(entity))
+                {
+                    entitiesFromOneParents.Add(entity);
+                }
+            }
+
+            else
+            {
+                newDict.Add(entity.ParentId, new List<Entity>() { entity });
+            }
+
+        }
+        return newDict;
+    }
+}
+
+public class Entity
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public int ParentId { get; set; }
+
+    public Entity(int Id, string Name, int ParentId)
+    {
+        this.Id = Id;
+        this.Name = Name;
+        this.ParentId = ParentId;
+    }
+}
---
+++ Task2_Generics/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+namespace Program;
+internal static class Program
+{
+    private static void Main()
+    {
+        string path = @"..\..\..\CheckingWork.txt";
+
+        LocalFileLogger<int> localFileLoggerInt = new LocalFileLogger<int>(path);
+        LocalFileLogger<string> localFileLoggerStr = new LocalFileLogger<string>(path);
+        LocalFileLogger<MyClass> localFileLoggerClass = new LocalFileLogger<MyClass>(path);
+
+        localFileLoggerInt.LogInfo("Info_Int"); localFileLoggerInt.LogWarning("Warning_Int"); localFileLoggerInt.LogError("Error_Int", new Exception());
+        localFileLoggerStr.LogInfo("Info_Str"); localFileLoggerStr.LogWarning("Warning_Str"); localFileLoggerStr.LogError("Error_Str", new NullReferenceException());
+        localFileLoggerClass.LogInfo("Info_My"); localFileLoggerClass.LogWarning("Warning_Class"); localFileLoggerClass.LogError("Error in MyClass", new DivideByZeroException());
+    }
+}
+
+interface ILogger
+{
+    public void LogInfo(string message);
+
+    public void LogWarning(string message);
+
+    public void LogError(string message, Exception ex);
+}
+
+public class LocalFileLogger<T> : ILogger
+{
+    private string path;
+
+    private string GenericTypeName = typeof(T).Name;
+
+    public LocalFileLogger(string path)
+    {
+        this.path = path;
+        File.WriteAllText(path, "");
+    }
+
+    public void LogInfo(string message)
+    {
+        File.AppendAllText(path, $"[Info]: [{GenericTypeName}] : {message}\n");
+    }
+
+    public void LogWarning(string message)
+    {
+        File.AppendAllText(path, $"[Warning] : [{GenericTypeName}] : {message}\n");
+    }
+
+    public void LogError(string message, Exception ex)
+    {
+        File.AppendAllText(path, $"[Error] : [{GenericTypeName}] : {message}. {ex.Message}\n");
+    }
+}
+
+public class MyClass
+{
+    private int number;
+    public MyClass()
+    {
+        number = 0;
+    }
+
+    public void Print()
+    {
+        Console.WriteLine(number);
+    }
+}
